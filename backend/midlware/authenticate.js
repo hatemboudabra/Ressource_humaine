d56299aa351204aaaa684757018738bf992bc648
dhia <dhiasmairi123@gmail.com>
2024-10-06 10:56:36 +0100

    Demandes de congés

--- backend/midlware/authenticate.js
+++ backend/midlware/authenticate.js
@@ -1,22 +1,23 @@
-const jwt = require ('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
+const authenticate = async (req, res, next) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) {
+        return res.status(401).json({ message: 'Accès refusé. Aucun token fourni.' });
+    }
 
-const authenticate = (req,res,next)=>{
-    const token = req.header('Authorization')
-    if(!token || !token.startsWith('Bearer ') ){
-        return res.status(401).send('Authentication failed:invalid token')
-}
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded._id);
+        if (!user) {
+            return res.status(401).json({ message: 'Utilisateur non trouvé.' });
+        }
+        req.user = user; // Ajoute l'utilisateur à la requête
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Token invalide.' });
+    }
+};
 
-try{
-    const tokenData = token.split(' ')[1];
-    const decodedToken = jwt.verify(tokenData,process.env.JWT_SECRET);
-    req.userId=decodedToken._id;
-    next();
-
-
-}catch (error){
-
-    return res.status(401).send('Authentication failed:invalid token ')
-}
-}
 module.exports = authenticate;
